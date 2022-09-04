ba7bdd2d2ee206a68c678b754c21b6593f0e0129
Alexander <renyreny125tt@gmail.com>
2022-09-04 16:26:39 +0300

    feat: add client microservice on gRPC

--- src/server.js
+++ src/server.js
@@ -1,11 +1,37 @@
 import Fastify from 'fastify'
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+import * as path from 'path'
+import * as url from 'url'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const PROTO_PATH = path.join(__dirname, "./proto/mailer.proto")
 
 const fastify = Fastify({
   logger: false
 })
 
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options)
+const Service = grpc.loadPackageDefinition(packageDefinition).MailService
+
+const client = new Service(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+)
+
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  client.send({ email: 'myemail@gmail.com', message: 'Hi microservice' }, (error, replyMicroservice) => {
+    if (error) throw error
+      reply.send(replyMicroservice.message)
+  })
 })
 
 const start = async () => {
